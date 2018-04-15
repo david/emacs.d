c68f61304756efdbd397203f231dfc6e6a4b96ab
David Leal <dleal@mojotech.com>
2018-04-15 16:25:32 +0100

    [modeline] Tweak telephone-line config.

--- init.el
+++ init.el
@@ -348,6 +348,17 @@
 
 (use-package telephone-line
   :config
+  (setq telephone-line-evil-use-short-tag t)
+  (setq telephone-line-lhs
+        '((evil   . (telephone-line-evil-tag-segment))
+          (accent . (telephone-line-projectile-segment
+                     telephone-line-process-segment))
+          (nil    . (telephone-line-buffer-segment))))
+  (setq telephone-line-primary-left-separator 'telephone-line-gradient
+        telephone-line-secondary-left-separator 'telephone-line-nil
+        telephone-line-primary-right-separator 'telephone-line-gradient
+        telephone-line-secondary-right-separator 'telephone-line-nil)
+
   (telephone-line-mode 1))
 
 (use-package uniquify
