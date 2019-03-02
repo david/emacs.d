d62ca78d9f03ace04950f9c23b7e05dd3b77db0a
David Leal <dleal@mojotech.com>
2019-03-02 16:34:06 +0000

    Reinstall flycheck.

--- init.el
+++ init.el
@@ -252,6 +252,8 @@
    "ctt" '(feature-verify-all-scenarios-in-buffer
            :which-key "run scenarios in buffer")))
 
+(use-package flycheck)
+
 (use-package flycheck-jest
   :disabled
   :after flycheck
@@ -628,3 +630,18 @@
          (t
           (rename-file filename new-name t)
           (set-visited-file-name new-name t t)))))))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (flycheck yasnippet yaml-mode xterm-color which-key wgrep use-package telephone-line stylus-mode smartparens rjsx-mode rainbow-mode rainbow-delimiters pug-mode projectile-rails prodigy prettier-js lsp-ui lsp-java json-mode highlight-indent-guides general feature-mode exec-path-from-shell evil-surround evil-string-inflection evil-matchit evil-magit evil-exchange evil-collection evil-args enh-ruby-mode emmet-mode dimmer dap-mode counsel-projectile company-lsp avy atom-one-dark-theme add-node-modules-path))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
